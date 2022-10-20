2f3ad995fa13cc0e3f506db7e896c7b0ea858d90
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-10-20 12:10:21 +0200

    Display score and game status. Allow users to change their names on the fly.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
 const player = (sign) => {
+    let _score = 0;
     const getSign = () => {
         return sign;
     }
-    return { getSign };   
+    const incrementScore = () => {
+        _score++;
+    }
+    return { getSign, incrementScore };   
 }
 
 const gameController = (() => {
@@ -18,7 +22,8 @@ const gameController = (() => {
         gameBoard.clearBoard();
         displayController.clearBoardDisplay();
         displayController.activateFields();
-        _roundNum = 1;        
+        _roundNum = 1;
+        displayController.showTurn();        
     }
 
     const _isGameOver = () => {
@@ -40,16 +45,20 @@ const gameController = (() => {
             }
             if (gameWon) {
                 if (getCurrentSign() === _player1.getSign()) {
-                    console.log("Player 1 wins");
+                    _player1.incrementScore();
+                    displayController.incrementScore(0);
+                    displayController.announceResult('player one');
                 } else {
-                    console.log("Player 2 wins");
+                    _player2.incrementScore();
+                    displayController.incrementScore(1);
+                    displayController.announceResult('player two');
                 }
                 return true;
             }
         }
 
         if (_roundNum === 9) {
-            console.log("It's a draw");
+            displayController.announceResult('draw');
             return true;
         }
     }
@@ -63,8 +72,10 @@ const gameController = (() => {
             displayController.markSign(indexX, indexY);
             if (_roundNum >= 5 && _isGameOver()) {
                 displayController.deactivateFields();
+            } else {
+                _roundNum++;
+                displayController.showTurn();
             }
-            _roundNum++;
         }
     }; 
     
@@ -75,12 +86,52 @@ const displayController = (() => {
     const _restartBtn = document.querySelector('.restart-button');
     const _divFields = Array.from( document.querySelectorAll('[class*="row"] [class*="column"]') );
     const _divsArray = new Array(3).fill("").map( () => new Array(3).fill(""));
+    const _playerScore = document.querySelectorAll('.score');
+    const _info = document.querySelector('.turn-info');
+    const _playerNames = document.querySelectorAll('.player > input');
 
     for (let i = 0; i < _divFields.length; i++) {
         _divsArray[Math.floor(i / 3)][i % 3] = _divFields[i];
     }
     _divsArray.forEach( row => row.forEach( div => div.addEventListener('click', gameController.addMark)));
     _restartBtn.addEventListener('click', gameController.resetGame);
+
+    const showTurn = () => {
+        if (gameController.getCurrentSign() === 'X') {
+            _info.textContent = `${_playerNames[0].value}, your turn`;
+        } else {
+            _info.textContent = `${_playerNames[1].value}, your turn`;
+        }
+    }
+    _playerNames.forEach( playerName => playerName.addEventListener('change', () => {
+        if (!playerName.value.trim()) playerName.value = playerName.defaultValue;
+
+        if (_info.textContent.includes("wins")) {
+            if ( (gameController.getCurrentSign() === 'X' && playerName.defaultValue === "Player one") ||
+                 (gameController.getCurrentSign() === 'O' && playerName.defaultValue === "Player two") ) {
+                let info = _info.textContent;
+                _info.textContent = info.replace(info.slice(0, info.lastIndexOf(" ")), playerName.value);
+            } 
+        } else if (_info.textContent.includes("turn")) showTurn();
+    }));
+
+    const markSign = (x, y) => {
+        _divsArray[x][y].textContent = gameController.getCurrentSign();
+    }
+
+    const incrementScore = index => {
+        _playerScore[index].textContent++;
+    }
+
+    const announceResult = (result) => {
+        if (result === "draw") {
+            _info.textContent = "It's a draw";
+        } else if (result === "player one") {
+            _info.textContent = `${_playerNames[0].value} wins`;
+        } else {
+            _info.textContent = `${_playerNames[1].value} wins`;
+        }
+    }
 
     const activateFields = () => {
         _divsArray.forEach( row => row.forEach( div => div.classList.remove('inactive')));
@@ -89,16 +140,18 @@ const displayController = (() => {
     const deactivateFields = () => {
         _divsArray.forEach( row => row.forEach( div => div.classList.add('inactive')));
     }
-
-    const markSign = (x, y) => {
-        _divsArray[x][y].textContent = gameController.getCurrentSign();
-    }
     
     const clearBoardDisplay = () => {
         _divsArray.forEach( row => row.forEach( div => div.textContent = ""));
     }
 
-    return { markSign, activateFields, deactivateFields, clearBoardDisplay }
+    return { markSign, 
+             showTurn, 
+             announceResult, 
+             incrementScore, 
+             activateFields, 
+             deactivateFields, 
+             clearBoardDisplay }
 })();
 
 const gameBoard = (() => {
