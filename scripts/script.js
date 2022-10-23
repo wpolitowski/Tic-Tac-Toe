f492ee608c2fb7075fb2656c99f7853f0aa364c5
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-10-23 16:33:21 +0200

    Fix changing the winner's name. Create AI opponent with random moves

--- scripts/script.js
+++ scripts/script.js
@@ -14,24 +14,30 @@ const gameController = (() => {
     const _player2 = player('O');
     let _roundNum = 1;
     let _gameNum = 1;
+    let _mode = "ai";
 
     const getCurrentSign = () => {
         if (_gameNum % 2) 
             return _roundNum % 2 ? _player1.getSign() : _player2.getSign();
         else 
             return _roundNum % 2 ? _player2.getSign() : _player1.getSign();
-    }   
+    }
+    
+    const _currentPlayer = () => {
+        if (getCurrentSign() === _player1.getSign()) return 1;
+        else return 2;
+    }
 
     const resetGame = () => {
         gameBoard.clearBoard();
         displayController.clearBoardDisplay();
         displayController.activateFields();
         _roundNum = 1;
-        displayController.showTurn();        
+        displayController.showTurn();
+        if (_mode === 'ai' && _currentPlayer() === 2) addMark();        
     }
 
     const _isGameOver = () => {
-        const arr = gameBoard.getGameBoard();
         const winners = [   ["0 0", "1 1", "2 2"],  ["0 2", "1 1", "2 0"],
                             ["0 0", "0 1", "0 2"],  ["1 0", "1 1", "1 2"],
                             ["2 0", "2 1", "2 2"],  ["0 0", "1 0", "2 0"],
@@ -42,7 +48,7 @@ const gameController = (() => {
             for (let coordinates of winner) {
                 let x = coordinates.slice(0,1);
                 let y = coordinates.slice(2);
-                if (arr[x][y] != getCurrentSign()) {
+                if (gameBoard.getField(x, y) != getCurrentSign()) {
                     gameWon = false;
                     break;
                 }              
@@ -67,22 +73,40 @@ const gameController = (() => {
         }
     }
 
-    const addMark = (e) => {      
-        let indexX = e.target.dataset.index.slice(0,1);
-        let indexY = e.target.dataset.index.slice(2);
+    const minimax = (board, player) => {
+        
+    }
 
-        if (gameBoard.isFieldEmpty(indexX, indexY)) {
-            gameBoard.markSign(indexX, indexY);
-            displayController.markSign(indexX, indexY);
+
+    const addMark = (e) => {    
+        let x, y;
+        if (_mode === "ai" && _currentPlayer() === 2) {
+            //random field will serve as "easy";
+            const availableFields = gameBoard.getAvailableFields();
+            const randInd = Math.floor(Math.random() * availableFields.length);
+            [x, y] = availableFields[randInd];
+
+
+        } else {
+            x = e.target.dataset.index.slice(0,1);
+            y = e.target.dataset.index.slice(2);
+        }
+        
+        if (gameBoard.isFieldEmpty(x, y)) {
+            gameBoard.markSign(x, y);
+            displayController.markSign(x, y);
             if (_roundNum >= 5 && _isGameOver()) {
                 displayController.deactivateFields();
                 _gameNum++;
             } else {
                 _roundNum++;
                 displayController.showTurn();
+                if (_mode === 'ai' && _currentPlayer() === 2) {
+                    addMark();
+                }
             }
         }
-    }; 
+    };     
     
     return { getCurrentSign, addMark, resetGame }
 })();
@@ -111,9 +135,11 @@ const displayController = (() => {
     _playerNames.forEach( playerName => playerName.addEventListener('change', () => {
         if (!playerName.value.trim()) playerName.value = playerName.defaultValue;
 
+        // after the game is over, _gameNum is incremented and turn order is switched
+        // if player one wins, then the current sign (for next game) is player's two sign
         if (_info.textContent.includes("wins")) {
-            if ( (gameController.getCurrentSign() === 'X' && playerName.defaultValue === "Player one") ||
-                 (gameController.getCurrentSign() === 'O' && playerName.defaultValue === "Player two") ) {
+            if ( (gameController.getCurrentSign() === 'O' && playerName.defaultValue === "Player one") ||
+                 (gameController.getCurrentSign() === 'X' && playerName.defaultValue === "Player two") ) {
                 let info = _info.textContent;
                 _info.textContent = info.replace(info.slice(0, info.lastIndexOf(" ")), playerName.value);
             } 
@@ -161,6 +187,7 @@ const displayController = (() => {
 
 const gameBoard = (() => {
     const _board = new Array(3).fill("").map( () => new Array(3).fill(""));
+
     const getGameBoard = () => {
         return _board;
     }
@@ -171,6 +198,20 @@ const gameBoard = (() => {
         _board[x][y] = gameController.getCurrentSign();
     }
 
+    const getField = (x, y) => {
+        return  _board[x][y];
+    }
+
+    const getAvailableFields = () => {
+        const available = [];
+        for (let i = 0; i < _board.length; i++) {
+            for (let j = 0; j < _board[i].length; j++) {
+                if (_board[i][j] === "") available.push([i,j]);
+            }
+        }
+        return available;
+    }
+
     const clearBoard = () => {
         for (let i = 0; i < _board.length; i++) {
             for (let j = 0; j < _board[i].length; j++) {
@@ -179,5 +220,5 @@ const gameBoard = (() => {
         }
     }
 
-    return { getGameBoard, isFieldEmpty, markSign, clearBoard };
+    return { getGameBoard, isFieldEmpty, markSign, getField, getAvailableFields, clearBoard };
 })();
